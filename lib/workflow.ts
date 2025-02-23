45e9622b34205288127bce433fd4eeb1b0b16482
LQCpaka <boyrovip1539@gmail.com>
2025-02-24 01:32:50 +0700

    fix qtash token

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -3,12 +3,11 @@ import { Client as QtashClient, resend } from "@upstash/qstash";
 import config from '@/lib/config'
 
 export const workflowClient = new WorkflowClient({
-
     baseUrl: config.env.uptash.qtashUrl,
     token: config.env.uptash.qtashToken,
 });
 
-const qtashClient = new QtashClient({ token: config.env.resendToken });
+const qtashClient = new QtashClient({ token: config.env.uptash.qtashToken });
 
 export const sendEmail = async ({ email, subject, message } : {email: string; subject: string; message: string;}) => {
     await qtashClient.publishJSON({
