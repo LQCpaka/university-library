7e36f6ab9eb1582c815f739e49c407b90a1c28ec
LQCpaka <boyrovip1539@gmail.com>
2025-03-01 16:26:15 +0700

    do a change

--- lib/actions/book.ts
+++ lib/actions/book.ts
@@ -1,4 +1,4 @@
-"use server";
+'use server'
 
 import { db } from "@/database/drizzle";
 import { books, borrowRecords } from "@/database/schema";
@@ -6,46 +6,39 @@ import { eq } from "drizzle-orm";
 import dayjs from "dayjs";
 
 export const borrowBook = async (params: BorrowBookParams) => {
-  const { userId, bookId } = params;
+    const { userId, bookId } = params;
 
-  try {
-    const book = await db
-      .select({ availableCopies: books.availableCopies })
-      .from(books)
-      .where(eq(books.id, bookId))
-      .limit(1);
+    try{
+        const book = await db.select({availableCopies: books.availableCopies}).from(books).where(eq(books.id, bookId)).limit(1);
 
-    if (!book.length || book[0].availableCopies <= 0) {
-      return {
-        success: false,
-        error: "Book is not available for borrowing",
-      };
+        if(!book.length || book[0].availableCopies <= 0){
+            return {
+                success: false,
+                error: 'Sách hiện không có sẵn để mượn'
+            }
+        } 
+        const dueDate = dayjs().add(7, 'day').toDate().toDateString();
+
+        const record = await db.insert(borrowRecords).values({
+            userId,
+            bookId,
+            dueDate,
+            status: 'BORROWED',
+
+        }); 
+
+        await db.update(books).set({availableCopies : book[0].availableCopies - 1}).where(eq(books.id, bookId));
+        
+        return {
+            success: true,
+            data: JSON.parse(JSON.stringify(record)),
+        };
+    } catch (error) {
+        console.error(error);
+
+        return {
+            success: false,
+            message: "Có lỗi đã xảy ra trong quá trình mượn sách"
+        };
     }
-
-    const dueDate = dayjs().add(7, "day").toDate().toDateString();
-
-    const record = await db.insert(borrowRecords).values({
-      userId,
-      bookId,
-      dueDate,
-      status: "BORROWED",
-    });
-
-    await db
-      .update(books)
-      .set({ availableCopies: book[0].availableCopies - 1 })
-      .where(eq(books.id, bookId));
-
-    return {
-      success: true,
-      data: JSON.parse(JSON.stringify(record)),
-    };
-  } catch (error) {
-    console.log(error);
-
-    return {
-      success: false,
-      error: "An error occurred while borrowing the book",
-    };
-  }
-};
+}
